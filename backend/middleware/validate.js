56bca2d6d9e347b9e83775e5031a360888543150
joselevelsup <joselevelsup@gmail.com>
2024-10-28 17:56:09 -0400

    forgot to add the validate starter code

---
+++ backend/middleware/validate.js
@@ -0,0 +1,8 @@
+//This function will be our middleware for helping validate request's body
+function validateBody(schema){
+  return function(req, res, next){
+    //Add code here
+  }
+}
+
+module.exports = validateBody;
